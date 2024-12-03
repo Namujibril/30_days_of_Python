9e43f1c3d31aae771c81b55bfa33690867a810c0
Namujibril <Namujibril@gmail.com>
2024-12-03 15:59:38 +0000

    commiting exercise1

---
+++ module_2/variables.py
@@ -0,0 +1,16 @@
+# Exercise level 1
+
+
+# Day 2: 30 Days of python programming
+first_name = "Nazifi"
+last_name = "Jibril"
+full_name = "Nazifi Jibril"
+country = "Nigeria"
+city = "Kano"
+age = 32
+year = 1992
+is_married = False
+is_true = True
+is_light_on = True
+
+first_name, last_name, age = "Nazifi", "Jibril", 32
